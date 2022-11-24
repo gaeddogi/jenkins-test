2edb60555ce50fc448ba1df0012bc6fc8affae4d
gaeddogi <dlgkdud7845@naver.com>
2022-11-24 17:42:23 +0900

    외부 tomcat war 배포설정 추가

--- src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
@@ -1,10 +1,18 @@
 package com.example.jenkinstest;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsTestApplication {
+public class JenkinsTestApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestApplication.class, args);
